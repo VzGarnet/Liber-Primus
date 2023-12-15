10f18144dbe04f529eab8f27d11532bc6eaa9ef5
Karen Agustine <karenagustine28@gmail.com>
2023-12-16 01:02:33 +0700

    forum

---
+++ resources/views/forum.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('head')
+    
+@endsection
+
+@section('content')
+<div class="bg-[#EDEDED] py-10">
+    <h1>Forum</h1>
+    <h5>Share your thoughts with others</h5>  
+    <div class="bg-white rounded-lg mx-5">
+        <div class="ml-20 flex py-20">
+            
+            
+                
+           
+        </div>
+    </div>
+
+</div>
+@endsection
