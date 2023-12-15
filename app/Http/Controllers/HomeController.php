fbd5372d42870741c6defd1e00b761f568a4dfb1
kevinds03 <kevindedy123@gmail.com>
2023-12-15 21:40:03 +0700

    HomePage

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Author;
+use App\Models\Book;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +25,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $books = Book::all();
+        $best_sellers = Book::orderBy('rating', 'desc')->take(5)->get();
+        $authors = Author::all();
+        return view('home', compact('books', 'best_sellers', 'authors'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,13 +20,13 @@
             <h1 class="font-bold text-2xl underline" style="text-decoration-color: #01C7C8;">Recommendation</h1>
             <a class="absolute top-0 right-20" href=""><h1 class="font-bold text-2xl underline text-[#01C7C8]">View All</h1></a>
         </div>                         
-        <div class="grid grid-cols-5 pb-5">
+        <div class="grid grid-cols-5 pb-5 gap-5 w-[90%]">
             @foreach ($books as $item)
-                <div class="flex flex-col text-left bg-white border border-black/[0.2] p-4 rounded-lg shadow-md" style="box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);">
-                    <img class='' src="{{($item->image)}}"width="150">
+                <div class="flex flex-col text-center bg-white border border-black/[0.2] p-4 rounded-lg shadow-md" style="box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);">
+                    <img class='w-[15vw] h-[40vh]' src="{{($item->image)}}"width="150">
                     <span class='font-semibold mt-1 text-sm text-[#888888]'>{{$item->authors->name}}</span>
                     <span class='font-semibold mt-1 text-sm text-black'>{{$item->title}}</span>
-                    <span class='font-bold mt-1 text-sm text-black'>Rp {{$item->price}}</span>
+                    <span class='font-bold mt-1 text-sm text-black'>Rp{{number_format($item->price, 2, ',', '.')}}</span>
                 </div>
             @endforeach
 
@@ -36,28 +36,32 @@
         <div class="flex pb-5 relative w-100 justify-center items-center">
             <h1 class="font-bold text-2xl underline" style="text-decoration-color: #01C7C8;">Best Seller</h1>
             <a class="absolute top-0 right-20" href=""><h1 class="font-bold text-2xl underline text-[#01C7C8]">View All</h1></a>
-        </div>  
-        <div class="grid grid-cols-5">
-            <div class="flex flex-col text-left bg-white border border-black/[0.2] p-4 rounded-lg shadow-md" style="box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);">
-                <img class='' src="{{url('/images/YangTelahLamaPergi.png')}}"width="150">
-                <span class='font-semibold mt-1 text-sm text-[#888888]'>Tere Liye</span>
-                <span class='font-semibold mt-1 text-sm text-black'>Yang Telah Lama Pergi</span>
-                <span class='font-bold mt-1 text-sm text-black'>Rp 51.000</span>
-            </div>
-        </div>
+        </div> 
+        <div class="grid grid-cols-5 gap-5 w-[90%]">
+            @foreach ($best_sellers as $item)
+                <div class="flex flex-col text-center bg-white border border-black/[0.2] p-4 rounded-lg shadow-md" style="box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);">
+                    <img class='w-[15vw] h-[40vh]' src="{{($item->image)}}"width="150">
+                    <span class='font-semibold mt-1 text-sm text-[#888888]'>{{$item->authors->name}}</span>
+                    <span class='font-semibold mt-1 text-sm text-black'>{{$item->title}}</span>
+                    <span class='font-bold mt-1 text-sm text-black'>Rp{{number_format($item->price, 2, ',', '.')}}</span>
+                </div>
+            @endforeach
+        </div> 
     </div>
     <div class="bg-[#EDEDED] flex flex-col justify-center items-center py-5">
-        <h1 class="font-bold text-2xl underline pb-5" style="text-decoration-color: #01C7C8;">Author Spotlight</h1>
-        <div class="grid grid-cols-2">
-            <div class="flex text-left rounded-3xl" style="box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);">
-                <img class='' src="{{url('/images/Leila Salikha.png')}}"width="150">
-                <div class="flex flex-col bg-white border w-80 border-black/[0.2] p-4 rounded-r-2xl shadow-md">
-                    <span class='font-semibold mt-1 text-xs text-[#888888]'>Name: </span>
-                    <span class='font-bold mt-1 text-sm text-black'>Leila Salikha Chudori</span>
-                    <span class='font-semibold mt-1 text-xs text-[#888888]'>About the author: </span>
-                    <span class='font-bold mt-1 text-sm text-black'>Lorem ipsum dolor sit amet,consectetur adipiscing elit. Maecenas vel felis massa.</span>
+        <h1 class="font-bold text-2xl underline pb-5 " style="text-decoration-color: #01C7C8;">Author Spotlight</h1>
+        <div class="grid grid-cols-2 gap-3">
+            @foreach ($authors as $author)
+                <div class="flex text-left rounded-3xl" style="box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);">
+                    <img class='rounded-l-2xl w-[15vw] h-[30vh]' src="{{$author->image}}"width="150">
+                    <div class="flex flex-col bg-white border w-80 border-black/[0.2] p-4 rounded-r-2xl shadow-md">
+                        <span class='font-semibold mt-1 text-xs text-[#888888]'>Name: </span>
+                        <span class='font-bold mt-1 text-sm text-black'>{{$author->name}}</span>
+                        <span class='font-semibold mt-1 text-xs text-[#888888]'>About the author: </span>
+                        <span class='font-bold mt-1 text-sm text-black'>Lorem ipsum dolor sit amet,consectetur adipiscing elit. Maecenas vel felis massa.</span>
+                    </div>
                 </div>
-            </div>
+            @endforeach
         </div>
     </div>
     <div class="bg-[#C6EFEF] flex flex-col justify-center items-center py-16">
