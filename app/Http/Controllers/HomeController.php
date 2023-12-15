b8e2d10bfef1fdc2cc960d040a6e7608abaab984
Karen Agustine <karenagustine28@gmail.com>
2023-12-15 21:59:33 +0700

    wishlistController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Book;
+use App\Models\Author;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $books = Book::all();
+        $best_sellers = Book::orderBy('rating', 'desc')->take(5)->get();
+        $authors = Author::all();
+        return view('home', compact('books', 'best_sellers', 'authors'));
     }
 }
--- app/Http/Controllers/WishlistController.php
+++ app/Http/Controllers/WishlistController.php
@@ -11,4 +11,23 @@ class WishlistController extends Controller
         $wishlists = Wishlist::all();
         return view('wishlist', compact('whislists'));
     }
+
+    public function test(Request $request, $id){
+      $user = $request->user()->id;
+      $wishlist = Wishlist::where('user_id', $user)->where('book_id',$id)->first();
+      
+      if($wishlist) {
+        $wishlist->delete(); 
+    } 
+    else{
+        Wishlist::create(
+            [
+                'user_id' => $user,
+                'book_id' => $id,
+            ]
+            );
+    }
+    return redirect()->back();
 }
+}
+   
