c05ff970e399939c32812849df9b96577ff567c7
Karen Agustine <karenagustine28@gmail.com>
2023-12-18 00:25:56 +0700

    image

--- resources/views/forum.blade.php
+++ resources/views/forum.blade.php
@@ -12,9 +12,9 @@
     </div>
     <div class="bg-white rounded-lg mx-5">
         <div class="ml-20 flex py-10 items-center">
-            <img class='w-[10vw] h-[20vh]' src="{{url('')}}">
+            <img class='w-[10vw] h-[20vh]' src="{{url('/images/Profile.png')}}">
             <div class="ml-10 w-full">
-                <form method="POST" action="" class="flex flex-col justify-center items-center"">
+                <form method="POST" action="" class="flex flex-col justify-center items-center">
                     @csrf
                     <input type="text" placeholder="Title" class="text-black border border-black rounded-md py-2 px-4 ml-4 mb-4 w-[80%] bg-[#767680] bg-opacity-0"/>
                     <input type="text" placeholder="Share your toughts here" class="text-black border border-black rounded-md pt-2 pb-5 px-4 ml-4 mb-4 w-[80%] bg-[#767680] bg-opacity-0"/>
@@ -25,7 +25,7 @@
     </div>
     <div class="bg-white rounded-lg mx-5 mt-10">
         <div class="ml-20 flex py-10 items-center">
-            <img class='w-[10vw] h-[20vh]' src="{{url('')}}">
+            <img class='w-[10vw] h-[20vh]' src="{{url('/images/Profile.png')}}">
             <div class="ml-10 w-full">
                 <h1 class="font-bold text-lg">Nyancet</h1>
                 <h1 class="font-semibold text-base mb-5">Posted on</h1>
