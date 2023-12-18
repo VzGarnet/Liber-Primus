38599ad8572b626f67a6902ff49afad1aee4eb03
kevinds03 <kevindedy123@gmail.com>
2023-12-18 23:27:59 +0700

    Tr Success

--- resources/views/transaction-success.blade.php
+++ resources/views/transaction-success.blade.php
@@ -20,13 +20,15 @@
 </style>
 
 <body>
-    <img id="logo" src="{{asset('/images/Logo.png')}}" alt="">
-    <h1 id="big-text">Thank you for your purchase!</h1>
-    <p id="small-text">Your items will be delivered soon</p>
+    <div class="text-center">
+        <img id="logo" src="{{asset('/images/Logo.png')}}" alt="">
+        <h1 id="big-text">Thank you for your purchase!</h1>
+        <p id="small-text">Your items will be delivered soon</p>
+    </div>
 
-    <div class="buttonsuccess">
-        <a href="/" class="btn-container bg-[]" id="btn1"><div class="texts-btn">Back to home page</div></a>
-        <a href="/#" class="btn-container" id="btn2"><div class="texts-btn">See transaction history</div></a>
+    <div class="text-center">
+        <a href="/" class="bg-[#2C393F] rounded-md" id="btn1"><div class="texts-btn">Back to home page</div></a>
+        <a href="/#" class="btn-container bg-[#2C393F] rounded-md" id="btn2"><div class="texts-btn">See transaction history</div></a>
     </div>
 </body>
 @endsection
